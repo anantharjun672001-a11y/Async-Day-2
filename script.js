298068bfce6960c0a08ad5b17ccbc1b1926308e6
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-28 10:40:07 +0530

    Fetch in async

--- script.js
+++ script.js
@@ -13,10 +13,20 @@
 
 //fetch in .then .catch
 
-const res = fetch("https://jsonplaceholder.typicode.com/users");
+/* const res = fetch("https://jsonplaceholder.typicode.com/users");
 //console.log(res);
 res.then((data)=>data.json())
 .then((result)=>console.log(result))
 .catch((error)=>console.log(error))
 
+ */
 
+// Fetch in async
+
+async function apicall() {
+    const res=await fetch("https://jsonplaceholder.typicode.com/users");
+    const data=await res.json();
+    console.log(data);
+    
+}
+apicall()
