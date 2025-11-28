a6f08ebc275392cdeb0e628d80a6d562a5a9200f
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-28 10:33:41 +0530

    fetch in .then .catch

--- script.js
+++ script.js
@@ -3,10 +3,20 @@
 
 //Fetch Using Methods
 // Syntax:
-    fetch("url",{
+   /*  fetch("url",{
         methods:"POST"/"GET"/"PUT"/"DELETE",
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify({
             ...
         })
-    })
+    }) */
+
+//fetch in .then .catch
+
+const res = fetch("https://jsonplaceholder.typicode.com/users");
+//console.log(res);
+res.then((data)=>data.json())
+.then((result)=>console.log(result))
+.catch((error)=>console.log(error))
+
+
