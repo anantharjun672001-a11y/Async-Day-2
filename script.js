ad62dea79820bfcbb89edaacf70c23cb95d70c92
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-28 10:12:01 +0530

    Fetch Using Methods Syntax

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //Fetch
 //  Syntax:fetch(url)
 
+//Fetch Using Methods
+// Syntax:
+    fetch("url",{
+        methods:"POST"/"GET"/"PUT"/"DELETE",
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({
+            ...
+        })
+    })
